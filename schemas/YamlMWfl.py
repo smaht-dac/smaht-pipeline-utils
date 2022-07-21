3905ad2e490d5a7c841f92e3b65f893467d7d8ca
Michele Berselli <berse@MacBook-Pro.local>
2022-07-21 13:36:18 -0400

    added initial schemas for YamlWfl and YamlMWfl

---
+++ schemas/YamlMWfl.py
@@ -0,0 +1,144 @@
+{
+    ## Schema #########################
+    '$schema': 'https://json-schema.org/draft/2020-12/schema',
+    '$id': '/schemas/YamlMWfl',
+    'title': 'YamlMWfl',
+    'description': 'Schema to validate a Yaml description of a MetaWorkflow',
+    'type': 'object',
+    'properties': {
+
+        ## MetaWorkflow information ###
+        'name': {
+            'description': 'Name of the MetaWorkflow',
+            'type': 'string'
+        },
+        'title': {
+            'description': 'Title of the MetaWorkflow',
+            'type': 'string'
+        },
+        'description': {
+            'description': 'Description of the MetaWorkflow',
+            'type': 'string'
+        },
+
+        ## General input information ##
+        'input': {
+            'description': 'Pipeline input, reference files, and general arguments',
+            'type': 'object',
+            'patternProperties': {
+                '.+': {'$ref': '/schemas/argument'}
+            }
+        },
+
+        # Workflows information ######
+        'workflows': {
+            'description': 'Workflows information and their dependencies',
+            'type': 'object',
+            'patternProperties': {
+                '.+': {
+                    'description': 'Workflow information',
+                    'type': 'object',
+                    'properties': {
+                        'input': {
+                            'description': 'Workflow input and dependencies information',
+                            'type': 'object',
+                            'patternProperties': {
+                                '.+': {'$ref': '/schemas/argument'}
+                            }
+                        },
+                        'output': {
+                            'description': 'Workflow output information',
+                            'type': 'object',
+                            'patternProperties': {
+                                '.+': {'$ref': '/schemas/argument-output'}
+                            }
+                        }
+                    },
+                    'required': ['input', 'output']
+                }
+            }
+        }
+
+
+    },
+    'required': ['name', 'description', 'input', 'workflows'],
+
+    ## Sub-schemas ####################
+    '$defs': {
+        'argument': {
+            '$schema': 'https://json-schema.org/draft/2020-12/schema',
+            '$id': '/schemas/argument',
+            'type': 'object',
+            'properties': {
+                'argument_type': {
+                    'type': 'string',
+                    'pattern': '^file\..+|^parameter\..+'
+                },
+                'dimensionality': {
+                    'type': 'number'
+                },
+                'files': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'string' # will need to change that to validate the right file format !!!!!
+                    }
+                },
+                'source': {
+                    'type': 'string'
+                },
+                'source_argument_name': {
+                    'type': 'string'
+                },
+                'scatter': {
+                    'type': 'number'
+                },
+                'gather': {
+                    'type': 'number'
+                },
+                'input_dimension': {
+                    'type': 'number'
+                },
+                'extra_dimension': {
+                    'type': 'number'
+                },
+                'mount': {
+                    'type': 'boolean'
+                },
+                'rename': {
+                    'type': 'string',
+                    'pattern': '^formula\:.+'
+                },
+                'unzip': {
+                    'type': 'string'
+                }
+            },
+            'required': ['argument_type']
+        },
+        'argument-output': {
+            '$schema': 'https://json-schema.org/draft/2020-12/schema',
+            '$id': '/schemas/argument-output',
+            'type': 'object',
+            'properties': {
+                'description': {
+                    'type': 'string'
+                },
+                'linkto_location': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'string'
+                    }
+                },
+                'file_type': {
+                    'type': 'string'
+                },
+                'higlass_file': {
+                    'type': 'boolean'
+                },
+                'variant_type': {
+                    'type': 'string'
+                }
+            },
+            'required': []
+        }
+    }
+}
---
+++ schemas/YamlWfl.py
@@ -0,0 +1,127 @@
+{
+    ## Schema #########################
+    '$schema': 'https://json-schema.org/draft/2020-12/schema',
+    '$id': '/schemas/YamlWfl',
+    'title': 'YamlWfl',
+    'description': 'Schema to validate a Yaml description of a Workflow',
+    'type': 'object',
+    'properties': {
+
+        ## Workflow information #######
+        'name': {
+            'description': 'Name of the Workflow',
+            'type': 'string'
+        },
+        'title': {
+            'description': 'Title of the Workflow',
+            'type': 'string'
+        },
+        'description': {
+            'description': 'Description of the Workflow',
+            'type': 'string'
+        },
+        'runner': {
+            'description': 'Workflow description in standard language',
+            'type': 'object',
+            'properties': {
+                'language': {
+                    'description': 'Language used in Workflow description',
+                    'type': 'string',
+                    'pattern': '[wW][dD][lL]|[cC][wW][lL]'
+                },
+                'main': {
+                    'description': 'Main description file',
+                    'type': 'string',
+                    'pattern': '.+\.cwl|.+\.wdl'
+                },
+                'child': {
+                    'description': 'Supplementary description files used by main',
+                    'type': 'array',
+                    'items': {
+                        'type': 'string',
+                        'pattern': '.+\.cwl|.+\.wdl'
+                    }
+                }
+            },
+            'required': ['language', 'main']
+        },
+        'software': {
+            'description': 'List of software used in the Workflow',
+            'type': 'array',
+            'items': {
+                'type': 'string'
+            }
+        },
+
+        ## Input information ##########
+        'input': {
+            'description': 'Input files and parameters',
+            'type': 'object',
+            'patternProperties': {
+                '.+': {'$ref': '/schemas/argument'}
+            }
+        },
+
+        ## Output information #########
+        'output': {
+            'description': 'Output files and quality controls',
+            'type': 'object',
+            'patternProperties': {
+                '.+': {'$ref': '/schemas/argument'}
+            }
+        }
+    },
+    'required': ['name', 'description', 'runner', 'input', 'output'],
+
+    ## Sub-schemas ####################
+    '$defs': {
+        'argument': {
+            '$schema': 'https://json-schema.org/draft/2020-12/schema',
+            '$id': '/schemas/argument',
+            'type': 'object',
+            'properties': {
+                'argument_type': {
+                    'type': 'string',
+                    'pattern': '^file\..+|^parameter\..+|^qc\..+'
+                },
+                'secondary_files': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'string'
+                    }
+                }
+            },
+            'required': ['argument_type'],
+
+            ## qc specific ############
+            'if': {
+                'type': 'object',
+                'properties': {
+                    'argument_type': {
+                        'pattern': '^qc\..+'
+                    }
+                },
+            },
+            'then': {
+                'properties': {
+                    'argument_to_be_attached_to': {
+                        'type': 'string'
+                    },
+                    'zipped': {
+                        'type': 'boolean'
+                    },
+                    'html': {
+                        'type': 'boolean'
+                    },
+                    'json': {
+                        'type': 'boolean'
+                    },
+                    'table': {
+                        'type': 'boolean'
+                    }
+                },
+                'required': ['argument_to_be_attached_to']
+            },
+        }
+    }
+}
