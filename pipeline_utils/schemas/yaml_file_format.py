e49096020c3a2bb604229e4492d4d6588585a137
Michele <berselli.michele@gmail.com>
2024-02-20 16:06:02 -0500

    Update pipeline_utils/schemas/yaml_file_format.py
    Co-authored-by: drio18 <58236592+drio18@users.noreply.github.com>

--- pipeline_utils/schemas/yaml_file_format.py
+++ pipeline_utils/schemas/yaml_file_format.py
@@ -25,7 +25,7 @@ yaml_file_format_schema = {
             schema.TYPE: schema.ARRAY,
             schema.ITEMS: {
                 schema.TYPE: schema.STRING,
-                schema.PATTERN: 'ReferenceFile|FileProcessed|FileSubmitted|FileFastq'
+                schema.PATTERN: 'ReferenceFile|OutputFile|AlignedReads|UnalignedReads|VariantCalls'
             }
         },
         'status': {
